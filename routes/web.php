e330ca221d6eb22f8374f6eabad0e7e324bd58eb
JustineEstrada <justineestrada96@gmail.com>
2020-12-04 00:06:16 +0800

    migration_for_attendants

--- routes/web.php
+++ routes/web.php
@@ -5,48 +5,11 @@ Route::get('/', function(){
 	return view('welcome');
 });
 
-Route::get('/templates', 'TemplatesController@index');
-Route::get('/templates/about', 'TemplatesController@about');
-Route::get('templates/sign-in', 'TemplatesController@signIn');
-
-//crud for amenity bookings
-//eloquent
-Route::get('/amebook', 'AmenityBookingsController@index');
-
-Route::get('/bookings/{book}', 'AmenityBookingsController@show');
-
-//crud
-Route::get('/bookings/create', 'AmenityBookingsController@create');
-
-Route::post('/bookings', 'AmenityBookingsController@store');
-
-Route::get('/bookings/{book}/edit', 'AmenityBookingsController@edit');
-
-Route::put('/bookings/{book}', 'AmenityBookingsController@update');	
-
-Route::delete('/bookings/{book}', 'AmenityBookingsController@destroy'); 
-
-
-//crud for Ship and Cruises
-Route::get('/ShipAndCruises','ShipAndCruisesController@index')
-//create
-Route::get('/ShipAndCruises/{ShipAndCruises}', 'ShipAndCruisesController@show');
-
-Route::get('/ShipAndCruises/create', 'ShipAndCruisesController@create');
-//store
-Route::post('/ShipAndCruises', 'ShipAndCruisesController@store');
-
-//up
-Route::get('/ShipAndCruises/{ShipAndCruises}/edit', 'ShipAndCruisesController@edit');
-
-Route::put('/ShipAndCruises/{ShipAndCruises}', 'ShipAndCruisesController@update');	
-
-//del
-
-Route::delete('/ShipAndCruises/{ShipAndCruises}', 'ShipAndCruisesController@destroy'); 
 
 
 
 
 
 
+
+	
