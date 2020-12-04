d044791a49b3e1c14099392761c0de211a120c1f
JustineEstrada <justineestrada96@gmail.com>
2020-12-04 10:27:23 +0800

    crud_for_attendants

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,18 @@ Route::get('/', function(){
 	return view('welcome');
 });
 
+Route::get('/templates', 'TemplatesController@index');
+Route::get('/templates/about', 'TemplatesController@about');
+Route::get('templates/sign-in', 'TemplatesController@signIn');
 
-
-
-
-
+//crud for attendants
+Route::get('/', 'AttendantsController@index');
+Route::get('/attendants/{attendant}', 'AttendantsController@show');
+Route::get('/attendants/{attendant}/edit', 'AttendantsController@edit');
+Route::get('/attendants/create', 'AttendantsController@create');
+Route::post('/attendants', 'AttendantsController@strore');
+Route::put('/attendants/{attendant}', 'AttendantsController@update');
+Route::delete('/attendants/{attendant}', 'AttendantsController@destroy');
 
 
 	
