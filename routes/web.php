893f3d7a58d1ce054464e052a1ebb4b4475e10b8
JustineEstrada <55123903+JustineEstrada@users.noreply.github.com>
2020-12-25 03:34:23 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,49 +4,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
 	return view('welcome');
 });
-
+//crude for attendant
+//seeder
 Route::get('/templates', 'TemplatesController@index');
 Route::get('/templates/about', 'TemplatesController@about');
 Route::get('templates/sign-in', 'TemplatesController@signIn');
 
-//crud for amenity bookings
-//eloquent
-Route::get('/amebook', 'AmenityBookingsController@index');
-
-Route::get('/bookings/{book}', 'AmenityBookingsController@show');
-
-//crud
-Route::get('/bookings/create', 'AmenityBookingsController@create');
-
-Route::post('/bookings', 'AmenityBookingsController@store');
-
-Route::get('/bookings/{book}/edit', 'AmenityBookingsController@edit');
-
-Route::put('/bookings/{book}', 'AmenityBookingsController@update');	
-
-Route::delete('/bookings/{book}', 'AmenityBookingsController@destroy'); 
-
-
-//crud for Ship and Cruises
-Route::get('/ShipAndCruises','ShipAndCruisesController@index')
-//create
-Route::get('/ShipAndCruises/{ShipAndCruises}', 'ShipAndCruisesController@show');
-
-Route::get('/ShipAndCruises/create', 'ShipAndCruisesController@create');
-//store
-Route::post('/ShipAndCruises', 'ShipAndCruisesController@store');
-
-//up
-Route::get('/ShipAndCruises/{ShipAndCruises}/edit', 'ShipAndCruisesController@edit');
-
-Route::put('/ShipAndCruises/{ShipAndCruises}', 'ShipAndCruisesController@update');	
-
-//del
-
-Route::delete('/ShipAndCruises/{ShipAndCruises}', 'ShipAndCruisesController@destroy'); 
-
-
-
-
-
-
+//crud for attendants
+Route::get('/', 'AttendantsController@index');
+Route::get('/attendants/{attendant}', 'AttendantsController@show');
+Route::get('/attendants/{attendant}/edit', 'AttendantsController@edit');
+Route::get('/attendants/create', 'AttendantsController@create');
+Route::post('/attendants', 'AttendantsController@strore');
+Route::put('/attendants/{attendant}', 'AttendantsController@update');
+Route::delete('/attendants/{attendant}', 'AttendantsController@destroy');
